61ec3dd857bb43a09296b5c63915480425a4f108
Amanda Lin <amandalincs@gmail.com>
2021-05-17 15:20:26 -0700

    Added functional button

--- demo.js
+++ demo.js
@@ -3,6 +3,10 @@ import {MyToolkit} from './mytoolkit.js';
 // Implement a MyToolkit Button
 var btn = new MyToolkit.Button;
 btn.move(100,100);
-btn.onclick(function(e){
-	console.log(e);
+btn.setLabel("Click Me!");
+btn.onClick(function(e){
+	console.log(e.type);
+});
+btn.onStateChange(function(e) {
+    console.log(e.type);
 });
--- mytoolkit.js
+++ mytoolkit.js
@@ -1,31 +1,67 @@
 import {SVG} from './svg.min.js';
 
+const teal = '#449D91';
+const light_teal = '#6EBEB3';
+const dark_teal = '#2B8A7D';
+
 var MyToolkit = (function() {
     var Button = function(){
         var draw = SVG().addTo('body').size('100%','100%');
-        var rect = draw.rect(100,50).fill('red')
-        var clickEvent = null
+        var group = draw.group();
 
-        rect.mouseover(function(){
-            this.fill({ color: 'blue'})
+        var rect = group.rect(125,50).fill(teal).radius(5);
+        var text = group.text("Button").font({
+            family: 'Gill Sans',
+            size: 18,
+            anchor: 'middle',
+            leading: '1.5em',
+            fill: "#ffffff"
+        }).center(rect.width()/2, rect.height()/2);
+
+        var clickEvent = null;
+        var stateChange = null;
+
+        rect.mouseover(function(event){
+            this.fill({ color: light_teal});
+            if (stateChange != null)
+                stateChange(event);
         })
-        rect.mouseout(function(){
-            this.fill({ color: 'red'})
+        
+        rect.mouseout(function(event){
+            this.fill({ color: teal});
+            if (stateChange != null)
+                stateChange(event);
         })
-        rect.mouseup(function(){
-            this.fill({ color: 'red'})
+
+        rect.mouseup(function(event){
+            this.fill({ color: teal});
+            if (stateChange != null)
+                stateChange(event);
         })
+
+        rect.mousedown(function(event){
+            this.fill({ color: dark_teal});
+            if (stateChange != null)
+                stateChange(event);
+        })
+
         rect.click(function(event){
-            this.fill({ color: 'pink'})
             if(clickEvent != null)
-                clickEvent(event)
+                clickEvent(event);
         })
+
         return {
             move: function(x, y) {
-                rect.move(x, y);
+                group.move(x, y);
             },
-            onclick: function(eventHandler){
-                clickEvent = eventHandler
+            onClick: function(eventHandler) {
+                clickEvent = eventHandler;
+            },
+            onStateChange: function(eventHandler) {
+                stateChange = eventHandler;
+            },
+            setLabel: function(newText) {
+                text.text(newText);
             }
         }
     }
