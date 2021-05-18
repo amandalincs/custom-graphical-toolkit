ac76f56e8c45cad8c107c98a37bdb695b9cc7e92
Amanda Lin <amandalincs@gmail.com>
2021-05-17 17:08:40 -0700

    Added functional checkbox

--- demo.js
+++ demo.js
@@ -2,11 +2,27 @@ import {MyToolkit} from './mytoolkit.js';
 
 // Implement a MyToolkit Button
 var btn = new MyToolkit.Button;
-btn.move(100,100);
+btn.move(100, 100);
 btn.setLabel("Click Me!");
 btn.onClick(function(e){
 	console.log(e.type);
 });
 btn.onStateChange(function(e) {
+    console.log(e.type);
+});
+
+// Implement a MyToolkit Checkbox
+var checkbox = new MyToolkit.Checkbox;
+checkbox.move(100, 50);
+checkbox.setLabel("Check Me!");
+checkbox.onCheck(function(checkStatus) {
+    console.log(checkStatus);
+    if (checkStatus == "unchecked") {
+        checkbox.setLabel("Check Me!");
+    } else if (checkStatus == "checked") {
+        checkbox.setLabel("Uncheck Me!");
+    }
+});
+checkbox.onStateChange(function(e) {
     console.log(e.type);
 });
--- mytoolkit.js
+++ mytoolkit.js
@@ -3,6 +3,8 @@ import {SVG} from './svg.min.js';
 const teal = '#449D91';
 const light_teal = '#6EBEB3';
 const dark_teal = '#2B8A7D';
+const white = '#FFFFFF';
+const yellow = '#FAC86D'
 
 var MyToolkit = (function() {
     var Button = function(){
@@ -15,7 +17,7 @@ var MyToolkit = (function() {
             size: 18,
             anchor: 'middle',
             leading: '1.5em',
-            fill: "#ffffff"
+            fill: white
         }).center(rect.width()/2, rect.height()/2);
 
         var clickEvent = null;
@@ -65,7 +67,81 @@ var MyToolkit = (function() {
             }
         };
     };
-return {Button}
+
+    var Checkbox = function() {
+        var draw = SVG().addTo('body').size('100%','100%');
+        var group = draw.group();
+
+        var rect = group.rect(20, 20).fill('#fff').radius(3);
+        rect.stroke({ color: teal, width: 3 });
+
+        var text = group.text("Checkbox").font({
+            family: 'Gill Sans',
+            size: 18,
+            fill: teal
+        }).attr({ x: 35, y: -7});
+
+        var checkEvent = null;
+        var stateChange = null;
+        var isChecked = false;
+
+        rect.mouseover(function(event) {
+            this.stroke({ color: light_teal });
+            if (stateChange != null)
+                stateChange(event);
+        });
+
+        rect.mouseout(function(event) {
+            this.stroke({ color: teal });
+            if (stateChange != null)
+                stateChange(event);
+        });
+
+        rect.mouseup(function(event){
+            this.stroke({ color: teal });
+            if (stateChange != null)
+                stateChange(event);
+        });
+
+        rect.mousedown(function(event){
+            if (stateChange != null)
+                stateChange(event);
+        });
+
+        rect.click(function(event) {
+            if (!isChecked) {
+                this.fill({ color: yellow});
+                isChecked = true;
+            } else {
+                this.fill({ color: white })
+                isChecked = false;
+            }
+
+            if (checkEvent != null) {
+                if (!isChecked) {
+                    checkEvent("unchecked");
+                } else {
+                    checkEvent("checked");
+                }
+            }
+        });
+
+        return {
+            move: function(x, y) {
+                group.move(x, y);
+            },
+            onCheck: function(eventHandler) {
+                checkEvent = eventHandler;
+            },
+            onStateChange: function(eventHandler) {
+                stateChange = eventHandler;
+            },
+            setLabel: function(newText) {
+                text.text(newText);
+            }
+        };
+    };
+return {Button, Checkbox}
 }());
 
 export{MyToolkit}
